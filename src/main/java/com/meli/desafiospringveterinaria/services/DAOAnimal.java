af58973a456bd306516a8d87b99bab32fdddf29a
Patricia Souza <patricia.asouza@mercadolivre.com>
2021-09-23 14:54:18 -0300

    atualizando classe DAOAnimal

--- src/main/java/com/meli/desafiospringveterinaria/services/DAOAnimal.java
+++ src/main/java/com/meli/desafiospringveterinaria/services/DAOAnimal.java
@@ -7,8 +7,12 @@ import java.util.List;
 public class DAOAnimal implements Persistivel<Animal> {
 
     @Override
-    public void cadastrar(Animal obj) {
-
+    public void cadastrar(Animal objAnimal) {
+        if (validaAnimal(objAnimal.getNumeroDoPaciente())){
+            cadastrar(objAnimal);
+        } else {
+            throw new RuntimeException("Animal já cadastrado.");
+        }
     }
 
     @Override
@@ -23,6 +27,16 @@ public class DAOAnimal implements Persistivel<Animal> {
 
     @Override
     public List<Animal> listagem() {
-        return null;
+        return listagem();
+    }
+
+    // validando se animal existe
+    private boolean validaAnimal(Integer numeroAnimal){
+        for(Animal animal: listagem()){
+            if (animal.getNumeroDoPaciente() == (numeroAnimal)){
+                return false;
+            }
+        }
+        return true;
     }
 }
